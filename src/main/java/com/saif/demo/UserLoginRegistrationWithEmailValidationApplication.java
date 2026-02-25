6b773631d6cc8e7ae2bbb0b250ea97c7b504ae42
saiflayouni <saiflayouni77@gmail.com>
2026-02-25 21:18:33 +0100

    chore: initial Spring Boot authentication project setup

---
+++ src/main/java/com/saif/demo/UserLoginRegistrationWithEmailValidationApplication.java
@@ -0,0 +1,13 @@
+package com.saif.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationWithEmailValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationWithEmailValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saif/demo/UserLoginRegistrationWithEmailValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationWithEmailValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
